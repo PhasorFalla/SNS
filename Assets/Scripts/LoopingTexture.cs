1afe6c19a519d99229275e6a6fe2bf978c35834b
Karina <36827712+Karina10135@users.noreply.github.com>
2018-09-25 12:24:46 +1200

    Looping texture

---
+++ Assets/Scripts/LoopingTexture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingTexture : MonoBehaviour
+{
+
+    public float speed;
+    public Vector2 offset;
+
+    private Material mat;
+    private const string tex = "_MainTex";
+
+    void Start()
+    {
+
+        mat = GetComponent<SpriteRenderer>().material;
+
+    }
+
+    void Update()
+    {
+        var x = offset.x += Time.deltaTime * speed;
+        var y = offset.y ;
+
+        mat.SetTextureOffset(tex, new Vector2(x, y));
+
+    }
+
+}
